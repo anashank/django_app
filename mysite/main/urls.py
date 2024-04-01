6026f2ccfdedaac4504d03ea8f4a179feeffa8a1
Anand Shankar <47243632+anashank@users.noreply.github.com>
2024-03-31 21:00:40 -0700

    API requests and web scraping

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("",views.index,name="index"),
+path("",views.v2,name="v2"),
+path("v3/",views.home,name="home"),
 path("<int:id>",views.index2,name="index2"),
-path("v1/",views.v1,name="v1"),
+path("v1/",views.v1,name="v1")
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
+from bs4 import BeautifulSoup
+import requests
 # Create your views here.
 
 def index(response):
@@ -12,3 +14,64 @@ def index2(response,id):
 
 def v1(response):
 	return HttpResponse("<h1>Second Django text</h1>")
+
+def v2(request):
+	query = 'anxiety'
+	url = f'https://psych2go.net/?s={query}&subset=posts&bp_search=1'
+	r = requests.get(url) # get the page content using requests
+	#print(r.content)
+	soup = BeautifulSoup(r.text, 'html.parser') # parse the page HTML
+	print(soup)
+
+	test = [li.find('a') for li in soup.findAll('li', {'class': 'bp-search-item bp-search-item_post has-access'})]
+	
+	data = []
+	for a in test:
+		data.append(a['href'])
+
+	#url = "https://jsearch.p.rapidapi.com/search"
+
+	#querystring = {"query":"Python developer in San Diego, USA","page":"1","num_pages":"1"}
+
+	# headers = {
+	# 	"X-RapidAPI-Key": "be340b85c9mshc362df34fc85300p13a52ajsn9d9de6c5339e",
+	# 	"X-RapidAPI-Host": "jsearch.p.rapidapi.com"
+	# }
+
+	#response = requests.get(url, headers=headers, params=querystring)
+
+	#print(response.json())
+
+	#details = response.json()
+
+	#data = details['data']
+
+	context = {
+			'data': data
+	}
+
+	return render(request,'articles/v2.html',context)
+
+def home(request):
+	url = "https://jsearch.p.rapidapi.com/search"
+
+	querystring = {"query":"Java developer in Los Angeles, USA","page":"1","num_pages":"1"}
+
+	headers = {
+		"X-RapidAPI-Key": "be340b85c9mshc362df34fc85300p13a52ajsn9d9de6c5339e",
+		"X-RapidAPI-Host": "jsearch.p.rapidapi.com"
+	}
+
+	response = requests.get(url, headers=headers, params=querystring)
+
+	#print(response.json())
+
+	details = response.json()
+
+	data = details['data']
+
+	context = {
+			'data': data
+	}
+
+	return render(request,'jobapi/home.html',context)
